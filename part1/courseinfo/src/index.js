d7564fac779edf58a92795937d3fa108d715ad15
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2021-02-06 07:09:54 +0000

    fixed typo error

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,50 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  //Header, Content, and Total
-  const Header = (courseName) => {
-    return <h1>{courseName} </h1>;
-  };
-  const Content = ({ parts }) => {
-    return (
-      <div>
-        <p>
-          {parts[0].name} {parts[0].exercises}
-        </p>
-        <p>
-          {parts[1].name} {parts[1].exercises}
-        </p>
-        <p>
-          {parts[2].name} {parts[2].exercises}
-        </p>
-      </div>
-    );
-  };
-  const Total = ({ parts }) => {
-    return (
+//Header, Content, and Total
+const Header = ({ courseName }) => {
+  return <h1>{courseName} </h1>;
+};
+const Content = ({ parts }) => {
+  return (
+    <div>
       <p>
-        Number of exercises{" "}
-        {parts[0].exercises + parts[1].exercisess + parts[2].exercises}
+        {parts[0].name} {parts[0].exercises}
       </p>
-    );
-  };
+      <p>
+        {parts[1].name} {parts[1].exercises}
+      </p>
+      <p>
+        {parts[2].name} {parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
 
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
-
+const course = {
+  name: "Half Stack application development",
+  parts: [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    { name: "State of a component", exercises: 14 },
+  ],
+};
+const App = () => {
   return (
     <div>
       <Header courseName={course.name} />
